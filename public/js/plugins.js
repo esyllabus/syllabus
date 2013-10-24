1fb6b78ce07ece06f67e53793261299b02787326
esyllabus <eb@syllabusny.com>
2013-10-23 21:16:06 -0400

    damon fixed waypoint thing

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -32,27 +32,38 @@ jQuery(document).ready(function ($) {
 
     //Cache some variables
     var links = $('.navigation').find('li');
+
+    links.data('no-highlight', '0');
+
+
     slide = $('.slide');
     button = $('.button');
     mywindow = $(window);
     htmlbody = $('html,body');
 
-
     //Setup waypoints plugin
-    slide.waypoint(function (event, direction) {
+    slide.waypoint(function (direction) {
 
+        if (links.data('no-highlight') === '1') { return; }
         //cache the variable of the data-slide attribute associated with each slide
-        dataslide = $(this).attr('data-slide');
+        var dataslide = $(this).attr('data-slide');
+
+        links.removeClass('active');
+        links.filter('[data-slide="' + dataslide + '"]').addClass('active');
 
         //If the user scrolls up change the navigation link that has the same data-slide attribute as the slide to active and 
         //remove the active class from the previous navigation link 
+        /*
         if (direction === 'down') {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').siblings().removeClass('active');
-}
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        // else If the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and 
+        //remove the active class from the next navigation link 
         else {
-            $('.navigation li[data-slide="' + (dataslide-1) + '"]').addClass('active').next().removeClass('active'); }
-
-                });
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+        */
+    });
 
     //waypoints doesnt detect the first slide when user scrolls back up to the top so we add this little bit of code, that removes the class 
     //from navigation link slide 2 and adds it to navigation link slide 1. 
@@ -77,8 +88,10 @@ jQuery(document).ready(function ($) {
     links.click(function (e) {
         e.preventDefault();
         dataslide = $(this).attr('data-slide');
-     //   $(this).addClass('active').siblings().removeClass('active');
+        links.removeClass('active').filter('[data-slide="' + dataslide + '"]').addClass('active');
         goToByScroll(dataslide);
+        links.data('no-highlight', '1');
+        window.setTimeout(function() { links.data('no-highlight', '0'); }, 2000);
     });
 
     //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
